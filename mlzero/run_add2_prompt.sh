c10c4d32125301797e5ea1d4f1cebf68d5ddd5b8
Anna Richter <38262584+anna-richter@users.noreply.github.com>
2026-05-06 09:32:59 +0200

    preparing new iteration with train and test set

--- mlzero/run_add2_prompt.sh
+++ mlzero/run_add2_prompt.sh
@@ -17,9 +17,9 @@ conda activate mlzero_env
 export OPENAI_API_KEY=$OPENAI_API_KEY
 
 srun mlzero \
-	--input /home/anri21/be-fair/mlzero/addition_2_data \
-	--config /home/anri21/be-fair/mlzero/conf.yaml \
+	--input /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/addition_2_data \
+	--config /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/conf.yaml \
 	--max-iterations 20 \
-	--output /home/anri21/be-fair/mlzero/${SLURM_ARRAY_TASK_ID}-addition_2 \
+	--output /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/${SLURM_ARRAY_TASK_ID}-addition_2 \
 	--verbosity 4 \
 	--continuous_improvement
--- mlzero/run_basic_prompt.sh
+++ mlzero/run_basic_prompt.sh
@@ -17,9 +17,9 @@ conda activate mlzero_env
 export OPENAI_API_KEY=$OPENAI_API_KEY
 
 srun mlzero \
-	--input /home/anri21/be-fair/mlzero/basic_prompt_data \
-	--config /home/anri21/be-fair/mlzero/conf.yaml \
+	--input /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/basic_prompt_data \
+	--config /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/conf.yaml \
 	--max-iterations 20 \
-	--output /home/anri21/be-fair/mlzero/${SLURM_ARRAY_TASK_ID}-basic_prompt \
+	--output /sc-projects/sc-proj-ikim-guidlight/be-fair/mlzero/${SLURM_ARRAY_TASK_ID}-basic_prompt \
 	--verbosity 4 \
 	--continuous_improvement
