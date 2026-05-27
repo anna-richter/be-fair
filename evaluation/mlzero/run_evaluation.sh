5d949011e873ee378ce7aea2d4a9f83f22a17891
Richter <anri21@s-sc-frontend1.charite.de>
2026-05-27 09:41:51 +0200

    mlzero evaluations on DDI completed successfully

--- evaluation/mlzero/run_evaluation.sh
+++ evaluation/mlzero/run_evaluation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --time=47:59:59
-#SBATCH --gres=gpu:1 
+#SBATCH --gres=gpu:2 
 #SBATCH --job-name=%a_evaluation
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=40G
-#SBATCH --partition=gpu
+#SBATCH --partition=pgpu
 #SBATCH --account=sc-users
 #SBATCH --output=%a_evaluation.o%j 
 #SBATCH --error=%a_evaluation.e%j
